b16ff7a8e9749aa5b2b3cf5d60712969e4efe8cf
zdeni sykorova <sykorova@hotmail.com>
2025-05-12 19:06:34 +0200

    motám se, nejdou mi pushnout Pocitac a Procesor

---
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -0,0 +1,79 @@
+package cz.czechitas.ukol3;
+
+import java.util.Objects;
+
+public class Pocitac {
+
+    private Procesor cpu;
+
+    public Procesor getCpu() {
+        return cpu;
+    }
+    public void setCpu(Procesor cpu) {
+        this.cpu = cpu;
+    }
+
+    private Pamet ram;
+
+    public Pamet getRam() {
+        return ram;
+    }
+
+    public void setRam(Pamet ram) {
+        this.ram = ram;
+    }
+
+    private Disk pevnyDisk;
+
+    public Disk getPevnyDisk() {
+        return pevnyDisk;
+    }
+
+    public void setPevnyDisk(Disk pevnyDisk) {
+        this.pevnyDisk = pevnyDisk;
+    }
+
+    boolean jeZapnuty = true;
+
+    public boolean isJeZapnuty() {
+        return jeZapnuty;
+    }
+
+    public void setJeZapnuty(boolean jeZapnuty) {
+        this.jeZapnuty = jeZapnuty;
+    }
+
+    @Override
+    public String toString() {
+        return "procesor " + cpu + "," + " pamet" + ram + " byte" + "," + " pevny disk" + pevnyDisk + "byte";
+    }
+
+
+
+
+    //to ale není, co je v úkolu
+    private long velikost;
+    public long getVelikost() {
+        return velikost;
+    }
+    public void setVelikost(long velikost) {
+        Objects.requireNonNull(velikost);
+        if (velikost < 0) {
+            System.err.println("Napiš velikost souboru.");
+        }
+        this.velikost = velikost;
+    }
+
+    //to ale není, co je v úkolu
+    public void vytvorSouborOVelikosti (long velikost, long vyuziteMisto, long kapacitaDisk) {
+
+        if (velikost + vyuziteMisto > kapacitaDisk) {
+            System.err.println("Kapacita disku je nedostatecna pro ulozeni souboru.");
+        }
+    }
+
+}
+
+
+
+
---
+++ src/main/java/cz/czechitas/ukol3/Procesor.java
@@ -0,0 +1,40 @@
+package cz.czechitas.ukol3;
+
+import java.util.Objects;
+
+public class Procesor {
+
+    private String nazevProcesor;
+
+    public String getNazevProcesor() {
+        return nazevProcesor;
+    }
+
+    public void setNazevProcesor(String nazevProcesor) {
+        Objects.requireNonNull(nazevProcesor);
+            if (nazevProcesor.isBlank()) {
+                System.err.println("Napis nazev procesoru.");
+                return;
+            }
+        this.nazevProcesor = nazevProcesor;
+    }
+
+    private long rychlost;
+
+    public long getRychlost() {
+        return rychlost;
+    }
+
+    public void setRychlost(long rychlost) {
+        Objects.requireNonNull(rychlost);
+        if (rychlost < 0) {
+            System.err.println("Musi byt vyplnena rychlost pocitace.");
+            return;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Procesor " + nazevProcesor + ";" + " rychlost " + rychlost + " byte";
+    }
+}
